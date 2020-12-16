154b616ba2f244d58a1056ba7d33c7d975989434
langlang.chen <langlang.chen@advantech.com.cn>
2020-12-16 15:35:29 +0800

    hook

--- hook.php
+++ hook.php
@@ -1,32 +1,32 @@
 <?php
 
-//密钥123456
-
-$secret = "123456"; //Github项目中对应的Secret
-
-//获取GitHub发送的内容
-$json = file_get_contents('php://input');
-
-$content = json_decode($json, true);
-
-//github发送过来的签名
-
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-
-if (!$signature) {
-  return http_response_code(404);
+/**
+ *git webhook 自动部署脚本
+ */
+//接收post参数
+$requestBody = file_get_contents("php://input");
+if (empty($requestBody)) {
+  exit('data null');
 }
+$content = json_decode($requestBody, true);
 
-list($algo, $hash) = explode('=', $signature, 2);
+//验证密码,验证码云上配置的webhook密码
+//if (empty($content['password']) || $content['password'] != 'password') //{
+//	exit('password error');
+//}
 
-//计算签名
-$payloadHash = hash_hmac($algo, $json, $secret);
-if ($hash !== $payloadHash) {
-  return http_response_code(404);
+$path = "/wwwroot/OneIndex"; //项目存放物理路径
+//判断master分支上是否有提交
+if ($content['ref'] == 'refs/heads/master' && $content['total_commits_count'] > 0) {
+  $res = shell_exec("cd {$path} && git pull origin master 2>&1"); //当前为www用户
+  $res_log = '------------------------->' . PHP_EOL;
+  $res_log .= '用户' . $content['user_name'] . ' 于' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . $content['total_commits_count'] . '个commit：' . PHP_EOL;
+  $res_log .= $res . PHP_EOL;
+  $x = file_put_contents("git_webhook_log.txt", $res_log, FILE_APPEND); //追加写入日志文件
+  if ($x) {
+    echo 'true-';
+  } else {
+    echo 'false-';
+  }
 }
-
-echo "开始部署<br>";
-
-$res = shell_exec("cd C:\wwwroot\OneIndex & git pull origin master");
-
-echo $res;
+echo 'done';
